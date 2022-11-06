893f9b5f9a672c29e9ff6f6a5eaf12f8bfba0227
Mateusz Machulik <mateusz.machulik94@gmail.com>
2022-11-06 14:26:14 +0100

    Dodaj pliki projektów.

---
+++ ZadanieDomowe4.13/FizzBuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieDomowe4._13
+{
+    public class FizzBuzz
+    {
+        public string GetFizzBuzz(int number)
+        {
+
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+            else if (number % 3 == 0)
+                return "Fizz";
+            else if (number % 5 == 0)
+                return "Buzz";
+      
+            return number.ToString();
+                
+        }
+    }
+}
+
---
+++ ZadanieDomowe4.13/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieDomowe4._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Program wyświetla FIZZ kiedy liczba jest podzielna przez 3, \nBUZZ kiedy liczb jest podzielna przez 5\nlub FIZZBUZZ jeżeli jest podzielna przez obie z nich.");
+            var fizzBuzz = new FizzBuzz();
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę: ");
+                var number = GetNumer();
+                Console.WriteLine(fizzBuzz.GetFizzBuzz(number));
+                Console.WriteLine();
+                Console.WriteLine("Jeżeli chcesz wyjść z aplikacji wciśnij 'X'");
+            }
+        }
+    public static int GetNumer()
+    {
+        while (true)
+        {
+                var value = Console.ReadLine();
+                if (value.ToUpper() == "X")
+                    Environment.Exit(0);
+            if (!int.TryParse(value, out int usersNumber))
+            {
+                Console.WriteLine("Wartość niepoprawna, podaj liczbę całkiwitą.");
+                continue;
+            }
+            return usersNumber;
+        }
+    }
+}
+}
